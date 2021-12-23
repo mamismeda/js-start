65c66babc498397eed7c1fe956e20bad02093292
mamismeda <amamismeda@gmail.com>
2021-12-23 19:56:26 +0400

    start learning js simple moments

---
+++ script.js
@@ -0,0 +1,17 @@
+let myscript ;
+
+myscript = " my first javascript example code "
+
+console.log(myscript.slice(9, -13));
+console.log(myscript.replace("first", "second"));
+console.log(myscript.split());
+console.log(myscript.search("code"));
+console.log(myscript.startsWith(" "));
+console.log(myscript.toUpperCase());
+console.log(myscript.trim());
+
+
+
+
+
+
