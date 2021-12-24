8ba3b2793eb95cc06466c7422f279afe921394ea
mamismeda <amamismeda@gmail.com>
2021-12-24 22:29:54 +0400

    rename of img in folder

--- script.js
+++ script.js
@@ -10,6 +10,52 @@ console.log(myscript.startsWith(" "));
 console.log(myscript.toUpperCase());
 console.log(myscript.trim());
 
+let pic = "images/layer 20.png"
+let tran = ` Michael Jordan `;
+let nodex = `NODIA U.P`;
+let interFace = `User interface designer and fornt-end developer`;
+let sage = `Massage`;
+let foly =`Following`;
+let skill =`SKILLS`;
+let Ui =`UI/UX`;
+let dev =`Front end development`;
+let tml = `HTML`;
+let sasi =`CSS`;
+let jasy =`Javascript`;
+let reac =`React`;
+let nody =`Node`;
+
+let botScript = `
+<div id="topScript" class="container d-flex justify-content-center align-items-center pt-5">
+<div class="col-5 text-white text-center top-diver">
+       <div class="prof">
+           <p>PRO</p>
+       </div>
+       <div class="pict m-auto mb-4">
+        <img class="img-fluid" src="" alt="wick">
+      </div>
+        <h3>${tran}</h3>
+        <h6>${nodex}</h6>
+        <p class="px-3"> ${interFace}</p>
+        <a class="atry" href="">${sage}</a>
+        <a class="atry" href="">${foly}</a>
+        <div>
+            <p class="skil mt-4">${skill}</p>
+         <div class="d-flex bot-diver">
+            <p class="me-4">${Ui}</p>
+            <p class="me-4">${dev}</p>
+            <p class="me-4">${tml}</p>
+            <p class="me-4">${sasi}</p>
+            <p class="me-4">${jasy}</p>
+            <p class="me-4">${reac}</p>
+            <p class="me-4">${nody}</p>
+        </div>
+     </div>
+</div>
+</div> 
+`
+topScript.innerHTML = botScript ;
+
 
 
 
